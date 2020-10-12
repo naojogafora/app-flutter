59dca12703d1c950d3fd1882bc1fe659aece01bc
George Rappel <george.concei@hotmail.com>
2020-10-12 18:45:42 -0300

    + Group Model - add fields for Ad and Member counts

--- lib/model/group.dart
+++ lib/model/group.dart
@@ -2,6 +2,7 @@ class Group {
   int id;
   String name, description;
   bool private, isModerator = false;
+  int adCount, memberCount;
 
   Group.fromJson(Map<String, dynamic> json) {
     this.id = json['id'];
@@ -9,5 +10,7 @@ class Group {
     this.description = json['description'];
     this.private = json['private'] == 0 ? false : true;
     this.isModerator = json['pivot'] != null && json['pivot']['is_moderator'] == 1 ? true : false;
+    this.adCount = json['ad_count'];
+    this.memberCount = json['member_count'];
   }
 }
