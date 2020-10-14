33d95a9d9c0caae641f46e29c8b21ffe67f16970
George Rappel <george.concei@hotmail.com>
2020-10-14 13:49:40 -0300

    + Ad Public List Request model

---
+++ lib/request/ads_public_list.dart
@@ -0,0 +1,25 @@
+import 'package:trocado_flutter/model/ad.dart';
+
+class AdsPublicListRequest {
+  int currentPage, lastPage, perPage, from, to, total;
+  String nextPageURL, prevPageURL, lastPageURL, firstPageURL;
+  List<Ad> data;
+
+  AdsPublicListRequest.fromJson(Map<String, dynamic> json){
+    this.currentPage = json["current_page"];
+    this.lastPage = json["last_page"];
+    this.perPage = json["per_page"];
+    this.from = json["from"];
+    this.to = json["to"];
+    this.total = json["total"];
+    this.nextPageURL = json["next_page_url"];
+    this.prevPageURL = json["prev_page_url"];
+    this.lastPageURL = json["last_page_url"];
+    this.firstPageURL = json["first_page_url"];
+
+    this.data = [];
+    for(dynamic adArray in json["data"]){
+      this.data.add(Ad.fromJson(adArray));
+    }
+  }
+}
