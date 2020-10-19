b01cbaa2539c0fb8ece11471a806e62085007168
George Rappel <george.concei@hotmail.com>
2020-10-19 09:02:28 -0300

    * Group model - change isMember field to get new attribute

--- lib/model/group.dart
+++ lib/model/group.dart
@@ -3,7 +3,7 @@ import 'package:trocado_flutter/response/ads_list.dart';
 class Group {
   int id;
   String name, description;
-  bool private, isModerator = false, isMember = false;
+  bool private = false, isModerator = false, isMember = false;
   int adCount, memberCount;
 
   /// Optional field, loaded only when the user sees the full group page.
@@ -17,6 +17,6 @@ class Group {
     this.isModerator = json['pivot'] != null && json['pivot']['is_moderator'] == 1 ? true : false;
     this.adCount = json['ad_count'];
     this.memberCount = json['member_count'];
-    this.isMember = json['pivot'] != null ? true : false;
+    this.isMember = json['is_member'];
   }
 }
