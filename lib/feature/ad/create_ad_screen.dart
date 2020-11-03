8502f34485db9ddbdcb76141efdfd93c3d0b285c
George Rappel <george.concei@hotmail.com>
2020-11-03 12:39:39 -0300

    + Ad create screen - image picker

--- lib/feature/ad/create_ad_screen.dart
+++ lib/feature/ad/create_ad_screen.dart
@@ -1,6 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:trocado_flutter/config/style.dart';
+import 'package:trocado_flutter/feature/addresses/address_provider.dart';
+import 'package:trocado_flutter/feature/group/groups_provider.dart';
 import 'package:trocado_flutter/model/ad.dart';
+import 'package:trocado_flutter/model/address.dart';
+import 'package:trocado_flutter/model/group.dart';
+import 'package:trocado_flutter/model/photo.dart';
+import 'package:trocado_flutter/widget/dialog.dart';
 import 'package:trocado_flutter/widget/trocado_app_bar.dart';
+import 'package:image_picker/image_picker.dart';
+
+const double IMAGE_SIZE = 65;
 
 class CreateAdScreen extends StatefulWidget {
   final Ad existingAd;
@@ -12,18 +25,74 @@ class CreateAdScreen extends StatefulWidget {
 }
 
 class _CreateAdScreenState extends State<CreateAdScreen> {
-
   Ad ad;
-  Widget _body;
+  List<Widget> _body;
   int _step;
   bool editing;
+  List<File> newImages = [];
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final picker = ImagePicker();
+
+  int totalImageCount() {
+    int count = newImages.length;
+
+    if (ad.photos != null) {
+      count += ad.photos.length;
+    }
+
+    return count;
+  }
+
+  List<Widget> imageList() {
+    List<Widget> imageList = [];
+
+    if (ad != null && ad.photos != null)
+      for (Photo photo in ad.photos) {
+        imageList.add(GestureDetector(
+          child: Image.network(
+            photo.url,
+            width: IMAGE_SIZE,
+            height: IMAGE_SIZE,
+          ),
+          onTap: () => showRemoveDialog(() {
+            ad.photos.remove(photo);
+            Navigator.of(context).pop();
+            setState(() {});
+          }),
+        ));
+      }
+
+    if (newImages != null)
+      for (File file in newImages) {
+        imageList.add(GestureDetector(
+          child: Image.asset(
+            file.path,
+            width: IMAGE_SIZE,
+            height: IMAGE_SIZE,
+          ),
+          onTap: () => showRemoveDialog(() {
+            newImages.remove(file);
+            Navigator.of(context).pop();
+            setState(() {});
+          }),
+        ));
+      }
+
+    return imageList;
+  }
+
+  void showRemoveDialog(Function() callback) {
+    DefaultDialog.show(context,
+        title: "Apagar Imagem",
+        message: "Deseja apagar essa imagem do anúncio?",
+        okCallback: callback);
+  }
 
   @override
   void initState() {
-    _body = _adStep1(context);
     _step = 1;
 
-    if(widget.existingAd == null){
+    if (widget.existingAd == null) {
       ad = Ad();
       editing = false;
     } else {
@@ -32,23 +101,43 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
     }
 
     super.initState();
+
+    Provider.of<AddressProvider>(context, listen: false)
+        .loadUserAddresses(context);
+    Provider.of<GroupsProvider>(context, listen: false).loadUserGroups(context);
   }
 
-  void nextPage(BuildContext context){
-    switch(_step){
+  List<Widget> buildCurrentPage(BuildContext context) {
+    switch (_step) {
+      case 1:
+        return _adStep1(context);
+        break;
+      case 2:
+        return _adStep2(context);
+        break;
+      case 3:
+        return _adStep3(context);
+        break;
+      default:
+        return null;
+        break;
+    }
+  }
+
+  void nextPage(BuildContext context) {
+    if (!_formKey.currentState.validate()) {
+      return;
+    }
+
+    _formKey.currentState.save();
+
+    switch (_step) {
       case 1:
         _step = 2;
-        _body = _adStep2(context);
         setState(() {});
         break;
       case 2:
         _step = 3;
-        _body = _adStep3(context);
-        setState(() {});
-        break;
-      case 3:
-        _step = 4;
-        _body = _adStep4(context);
         setState(() {});
         break;
       default:
@@ -56,14 +145,15 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
     }
   }
 
-  void previousPage(BuildContext context){
-    switch(_step){
+  void previousPage(BuildContext context) {
+    switch (_step) {
       case 1:
         Navigator.of(context).pop();
         break;
       case 2:
         _step = 1;
         _body = _adStep1(context);
+        ad.photoFiles = newImages;
         setState(() {});
         break;
       case 3:
@@ -71,75 +161,269 @@ class _CreateAdScreenState extends State<CreateAdScreen> {
         _body = _adStep2(context);
         setState(() {});
         break;
-      case 4:
-        _step = 3;
-        _body = _adStep3(context);
-        setState(() {});
-        break;
       default:
         break;
     }
   }
 
+  Future getImage(ImageSource imageSource) async {
+    final pickedFile = await picker.getImage(
+        source: imageSource, imageQuality: 85, maxHeight: 1000, maxWidth: 1000);
+
+    setState(() {
+      if (pickedFile != null) {
+        newImages.add(File(pickedFile.path));
+        setState(() {});
+      } else {
+        print('No image selected.');
+      }
+    });
+  }
+
   Future<bool> submit() async {
+    print("Enviando anuncio:");
+    print(ad);
+    print("Como json:");
+    print(ad.toJson());
+
     try {
       //TODO Send to the API
+      return true;
     } catch (e) {
       //TODO Just throws exceptions, if any.
     }
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
+    _body = buildCurrentPage(context);
+
     return Scaffold(
-      appBar: trocadoAppBar("Novo Anúncio"),
-      body: _body,
+      appBar: trocadoAppBar("Detalhes do Anúncio"),
+      body: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                autovalidateMode: AutovalidateMode.always,
+                key: _formKey,
+                child: ListView(
+                  children: _body,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
   // Title and Description
-  Widget _adStep1(BuildContext context) {
-    return Container();
-  }
-
-  // Photo Selection
-  Widget _adStep2(BuildContext context) {
-    return Container();
+  List<Widget> _adStep1(BuildContext context) {
+    return [
+      TextFormField(
+        validator: (value) =>
+            value.isEmpty || value.length < 3 || value.length > 150
+                ? "Deve ter de 3 a 150 caracteres"
+                : null,
+        decoration: InputDecoration(
+            hintText: "Ex: Mesa de Madeira...",
+            labelText: "Título",
+            icon: Icon(Icons.short_text)),
+        initialValue: ad.title,
+        onSaved: (val) => ad.title = val,
+      ),
+      TextFormField(
+        validator: (value) =>
+            value.isEmpty || value.length < 20 || value.length > 500
+                ? "Deve ter de 20 a 500 caracteres"
+                : null,
+        decoration: InputDecoration(
+            hintText: "Fale sobre seu item...",
+            labelText: "Descrição",
+            icon: Icon(Icons.description)),
+        initialValue: ad.description,
+        onSaved: (val) => ad.description = val,
+        maxLines: 3,
+      ),
+      Padding(
+        padding: const EdgeInsets.only(top: 12, bottom: 6),
+        child: Text("Fotos (Até 5 imagens)"),
+      ),
+      totalImageCount() < 5
+          ? Expanded(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 8),
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTap: () => getImage(ImageSource.gallery),
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          color: Colors.grey[300],
+                          alignment: Alignment.center,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(Icons.add),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text("Galeria"),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    VerticalDivider(
+                      width: 6,
+                    ),
+                    Expanded(
+                      flex: 1,
+                      child: GestureDetector(
+                        onTap: () => getImage(ImageSource.camera),
+                        child: Container(
+                          padding: EdgeInsets.all(8),
+                          color: Colors.grey[300],
+                          alignment: Alignment.center,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(Icons.add_a_photo),
+                              Padding(
+                                padding:
+                                    const EdgeInsets.symmetric(horizontal: 8.0),
+                                child: Text("Câmera"),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          : Container(),
+      Row(
+        mainAxisSize: MainAxisSize.max,
+        children: imageList(),
+      ),
+      RaisedButton(
+        child: Text("Continuar"),
+        onPressed: () => nextPage(context),
+        padding: EdgeInsets.all(14),
+        color: Style.accentColor,
+        textColor: Style.clearWhite,
+      ),
+    ];
   }
 
   // Groups and Addresses selection
-  Widget _adStep3(BuildContext context) {
-    return Container();
+  List<Widget> _adStep2(BuildContext context) {
+    AddressProvider addressProvider =
+        Provider.of<AddressProvider>(context, listen: true);
+    GroupsProvider groupsProvider =
+        Provider.of<GroupsProvider>(context, listen: true);
+
+    return [
+      Text("Endereços de Retirada do Item (visível apenas para o Receptor)"),
+      Container(
+        height: 180,
+        child: ListView.builder(
+          itemCount: addressProvider.userAddresses.length,
+          itemBuilder: (context, i) {
+            Address address = addressProvider.userAddresses[i];
+            return CheckboxListTile(
+              value: ad.addresses.contains(address),
+              onChanged: (bool checked) {
+                if(checked){
+                  ad.addAddress(address);
+                } else {
+                  ad.removeAddress(address);
+                }
+                setState(() {});
+              },
+              title: Text(address.title),
+              subtitle: Text(address.street),
+            );
+          },
+        ),
+      ),
+      Text("Grupos Disponíveis"),
+      Container(
+        height: 180,
+        child: ListView.builder(
+          itemCount: groupsProvider.userGroups.length,
+          itemBuilder: (context, i) {
+            Group group = groupsProvider.userGroups[i];
+            return CheckboxListTile(
+              value: ad.containsGroup(group),
+              onChanged: (bool checked) {
+                if(checked){
+                  ad.addGroup(group);
+                } else {
+                  ad.removeGroup(group);
+                }
+                setState(() {});
+              },
+              title: Text(group.name),
+            );
+          },
+        ),
+      ),
+      Divider(),
+      RaisedButton(
+        child: Text("Publicar Anúncio"),
+        onPressed: () => nextPage(context),
+        padding: EdgeInsets.all(14),
+        color: Colors.green,
+        textColor: Style.clearWhite,
+      ),
+      RaisedButton(
+        child: Text("Voltar"),
+        onPressed: () => previousPage(context),
+        color: Colors.orange[100],
+      ),
+    ];
   }
 
   // Loading and Success message
-  Widget _adStep4(BuildContext context) {
-    return Center(
-        child: FutureBuilder<bool>(
-          future: submit(),
-          builder: (context, snapshot){
-            if(!snapshot.hasData && !snapshot.hasError){
-              return CircularProgressIndicator();
-            }
+  List<Widget> _adStep3(BuildContext context) {
+    return [
+      FutureBuilder<bool>(
+        future: submit(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData && !snapshot.hasError) {
+            return CircularProgressIndicator();
+          }
 
-            if(snapshot.hasError){
-              Future.delayed(Duration(seconds: 3), () {
-                // 5s over, navigate to a new page
-                previousPage(context);
-              });
-              //TODO Show error message
-              return Icon(Icons.close, color: Colors.red);
-            }
-
+          if (snapshot.hasError) {
             Future.delayed(Duration(seconds: 3), () {
               // 5s over, navigate to a new page
-              Navigator.of(context).pop();
+              previousPage(context);
             });
+            //TODO Show error message
+            return Icon(Icons.close, color: Colors.red);
+          }
 
-            //TODO Show success message
-            return Icon(Icons.check_circle, color: Colors.green);
-          },
-        )
-    );
+          Future.delayed(Duration(seconds: 2), () {
+            // 5s over, navigate to a new page
+            Navigator.of(context).pop();
+          });
+
+          //TODO Show success message
+          return Icon(Icons.check_circle, color: Colors.green);
+        },
+      )
+    ];
   }
 }
--- lib/model/ad.dart
+++ lib/model/ad.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:trocado_flutter/model/photo.dart';
 import 'package:trocado_flutter/model/user.dart';
+
+import 'address.dart';
+import 'group.dart';
 
 class Ad {
   int id;
@@ -8,10 +13,16 @@ class Ad {
   String title, description;
   List<Photo> photos;
   DateTime createdAt, updatedAt;
+  List<Address> addresses;
+  List<Group> groups;
+  List<File> photoFiles;
 
   get firstPhoto => photos != null && photos.length > 0 ? photos[0] : null;
 
   Ad(){
+    photos = [];
+    addresses = [];
+    groups = [];
     finished = false;
     suspended = false;
   }
@@ -30,5 +41,55 @@ class Ad {
     for(dynamic photoArray in json['photos']){
       photos.add(Photo.fromJson(photoArray));
     }
+  }
+
+  Map<String, dynamic> toJson() {
+    Map<String, dynamic> json = {
+    'title': this.title,
+    'description': this.description,
+    'finished': this.finished,
+    'suspended': this.suspended
+    };
+
+    for(int i = 0; i < groups.length; i++) {
+      json.addAll({ "group_ids[$i]": groups[i].id });
+    }
+
+    for(int i = 0; i < addresses.length; i++) {
+      json.addAll({ "address_ids[$i]": addresses[i].id });
+    }
+
+    for(int i = 0; i < photoFiles.length; i++) {
+      json.addAll({ "photos[$i]": photoFiles[i] });
+    }
+
+    return json;
+  }
+
+  void addAddress(Address address){
+    if(!containsAddress(address))
+      addresses.add(address);
+  }
+
+  void removeAddress(Address address){
+    if(containsAddress(address))
+      addresses.removeWhere((element) => element.id == address.id);
+  }
+
+  bool containsAddress(Address address){
+    return addresses.any((element) => element.id == address.id);
+  }
+
+  void addGroup(Group group){
+    if(!containsGroup(group))
+      groups.add(group);
+  }
+  void removeGroup(Group group){
+    if(containsGroup(group))
+      groups.removeWhere((element) => element.id == group.id);
+  }
+
+  bool containsGroup(Group group){
+    return groups != null && groups.any((element) => element.id == group.id);
   }
 }
