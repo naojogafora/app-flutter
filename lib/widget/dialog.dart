ac500a8333c84694f785737f97dfebb8bf3702f1
George Rappel <george.concei@hotmail.com>
2020-11-03 12:37:15 -0300

    * Button calllback to default dialog

--- lib/widget/dialog.dart
+++ lib/widget/dialog.dart
@@ -3,9 +3,10 @@ import 'package:flutter/material.dart';
 class DefaultDialog {
   String message;
   String title;
+  String okButtonText;
   BuildContext context;
 
-  DefaultDialog.show(context, {message, title, okCallback}){
+  DefaultDialog.show(context, {message, title, okCallback, okButtonText}){
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -14,7 +15,17 @@ class DefaultDialog {
           content: Text(message),
           actions: [
             GestureDetector(
-              child: Text("OK"),
+              child: Container(
+                padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 20.0),
+                child: Text("Cancelar"),
+              ),
+              onTap: () => Navigator.of(context).pop(),
+            ),
+            GestureDetector(
+              child: Container(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(okButtonText != null && okButtonText.isNotEmpty ? okButtonText : "OK"),
+              ),
               onTap: okCallback != null ? okCallback : () => defaultCallback(context),
             ),
           ],
