0c16263a753e2c5da1117fce16361f6ed0c06470
George Rappel <george.concei@hotmail.com>
2020-11-24 11:41:13 -0300

    + Ad - Photo Gallery
    Took 38 minutes

--- lib/feature/ad/ad_details_screen.dart
+++ lib/feature/ad/ad_details_screen.dart
@@ -1,87 +1,108 @@
 import 'package:flutter/material.dart';
 import 'package:trocado_flutter/config/style.dart';
+import 'package:trocado_flutter/feature/ad/ad_overlay_gallery.dart';
 import 'package:trocado_flutter/model/ad.dart';
 
-class AdDetailsScreen extends StatelessWidget {
+class AdDetailsScreen extends StatefulWidget {
   final Ad ad;
   AdDetailsScreen(this.ad);
 
+  @override
+  _AdDetailsScreenState createState() => _AdDetailsScreenState();
+}
+
+class _AdDetailsScreenState extends State<AdDetailsScreen> {
+  bool showingGallery = false;
+
   void _onTapImageCount() {
-    //TODO
+    setState(() {
+      showingGallery = true;
+    });
+  }
+
+  void closeGalleryCallback(){
+    setState(() {
+      showingGallery = false;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            floating: true,
-            pinned: true,
-            title: ad.firstPhoto != null ? null : const Icon(Icons.no_photography_outlined),
-            expandedHeight: ad.firstPhoto != null ? 240 : 60,
-            flexibleSpace: ad.firstPhoto != null
-                ? Stack(
-                    children: [
-                      Row(
-                        mainAxisSize: MainAxisSize.max,
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: [
+              SliverAppBar(
+                floating: true,
+                pinned: true,
+                title: widget.ad.firstPhoto != null ? null : const Icon(Icons.no_photography_outlined),
+                expandedHeight: widget.ad.firstPhoto != null ? 240 : 60,
+                flexibleSpace: widget.ad.firstPhoto != null
+                    ? Stack(
                         children: [
-                          Expanded(
-                              child: Hero(
-                                  tag: "ad-image-" + ad.id.toString(),
-                                  child: Image.network(ad.firstPhoto.url, fit: BoxFit.cover))),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: GestureDetector(
-                          child: Container(
-                            padding: EdgeInsets.symmetric(vertical: 4, horizontal: 6),
-                            decoration: ShapeDecoration(
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.all(Radius.circular(14))),
-                                color: Colors.white54),
-                            child: Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                const Icon(
-                                  Icons.camera_alt,
-                                  size: 22,
-                                ),
-                                Padding(
-                                  padding: const EdgeInsets.all(2.0),
-                                  child: Text(
-                                    ad.photos.length.toString(),
-                                    style: const TextStyle(fontSize: 20),
-                                  ),
-                                ),
-                              ],
-                            ),
+                          Row(
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              Expanded(
+                                  child: Hero(
+                                      tag: "ad-image-" + widget.ad.id.toString(),
+                                      child: Image.network(widget.ad.firstPhoto.url, fit: BoxFit.cover))),
+                            ],
                           ),
-                          onTap: _onTapImageCount,
-                        ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: GestureDetector(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 4, horizontal: 6),
+                                decoration: ShapeDecoration(
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.all(Radius.circular(14))),
+                                    color: Colors.white54),
+                                child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  children: [
+                                    const Icon(
+                                      Icons.camera_alt,
+                                      size: 22,
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.all(2.0),
+                                      child: Text(
+                                        widget.ad.photos.length.toString(),
+                                        style: const TextStyle(fontSize: 20),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              onTap: _onTapImageCount,
+                            ),
+                          )
+                        ],
+                        alignment: Alignment.bottomRight,
                       )
-                    ],
-                    alignment: Alignment.bottomRight,
-                  )
-                : Container(),
-          ),
-          SliverList(
-            delegate: SliverChildListDelegate([
-              Container(
-                padding: const EdgeInsets.all(6),
-                child: Text(
-                  ad.title,
-                  style: const TextStyle(fontSize: 24),
-                  textAlign: TextAlign.center,
-                ),
+                    : Container(),
               ),
-              const Divider(
-                  color: Style.accentColor, height: 4, thickness: 2, indent: 20, endIndent: 20),
-            ]),
+              SliverList(
+                delegate: SliverChildListDelegate([
+                  Container(
+                    padding: const EdgeInsets.all(6),
+                    child: Text(
+                      widget.ad.title,
+                      style: const TextStyle(fontSize: 24),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  const Divider(
+                      color: Style.accentColor, height: 4, thickness: 2, indent: 20, endIndent: 20),
+                ]),
+              ),
+            ],
           ),
+          showingGallery ? AdOverlayGallery(widget.ad.photos, closeGalleryCallback) : Container(),
         ],
       ),
     );
---
+++ lib/feature/ad/ad_overlay_gallery.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:trocado_flutter/config/style.dart';
+import 'package:trocado_flutter/model/photo.dart';
+
+class AdOverlayGallery extends StatefulWidget {
+  final List<Photo> photos;
+  final VoidCallback closeCallback;
+
+  AdOverlayGallery(this.photos, this.closeCallback);
+
+  @override
+  _AdOverlayGalleryState createState() => _AdOverlayGalleryState();
+}
+
+class _AdOverlayGalleryState extends State<AdOverlayGallery> {
+  int currentPhoto;
+  List<Image> loadedImages = [];
+
+  @override
+  void initState() {
+    super.initState();
+    currentPhoto = 0;
+    widget.photos.forEach((e) => loadedImages.add(Image.network(e.url, fit: BoxFit.fitWidth)));
+  }
+
+  void previousPhoto() {
+    if (hasPreviousPhoto()) {
+      setState(() {
+        currentPhoto--;
+      });
+    }
+  }
+
+  void nextPhoto() {
+    if (hasNextPhoto()) {
+      setState(() {
+        currentPhoto++;
+      });
+    }
+  }
+
+  bool hasNextPhoto() => currentPhoto < widget.photos.length - 1;
+  bool hasPreviousPhoto() => currentPhoto > 0;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: widget.closeCallback,
+      child: Container(
+        padding: const EdgeInsets.all(24.0),
+        color: Colors.black54,
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height,
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Align(
+                child: IconButton(
+                    icon: Icon(Icons.close),
+                    onPressed: widget.closeCallback,
+                    iconSize: 42,
+                    color: Style.clearWhite,),
+                alignment: Alignment.centerRight,
+              ),
+              Expanded(
+                child: GestureDetector(
+                  child: Container(child: loadedImages[currentPhoto], color: Style.primaryColor),
+                  onTap: () {},
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      icon: Icon(Icons.keyboard_arrow_left),
+                      onPressed: previousPhoto,
+                      iconSize: 56,
+                      color: hasPreviousPhoto() ? Style.clearWhite : Colors.white38),
+                  Text((currentPhoto+1).toString()  + "/" + widget.photos.length.toString(), style: TextStyle(color: Style.clearWhite, fontSize: 16),),
+                  IconButton(
+                      icon: Icon(Icons.keyboard_arrow_right),
+                      onPressed: nextPhoto,
+                      iconSize: 56,
+                      color: hasNextPhoto() ? Style.clearWhite : Colors.white38),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
