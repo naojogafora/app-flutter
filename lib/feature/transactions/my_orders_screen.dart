726e580037ef97defa776482ff70785c697936b5
George Rappel <george.concei@hotmail.com>
2020-11-27 18:49:56 -0300

    * Transactions - change orders list to FutureBuilder with loading indicator
    Took 17 minutes

--- lib/feature/transactions/my_orders_screen.dart
+++ lib/feature/transactions/my_orders_screen.dart
@@ -2,32 +2,48 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:trocado_flutter/feature/transactions/transaction_list_tile.dart';
 import 'package:trocado_flutter/feature/transactions/transactions_provider.dart';
+import 'package:trocado_flutter/response/transaction_list.dart';
 import 'package:trocado_flutter/widget/trocado_app_bar.dart';
 
 class MyOrdersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TransactionsProvider provider = Provider.of<TransactionsProvider>(context);
+    
     return Scaffold(
       appBar: trocadoAppBar("Meus Pedidos"),
-      body: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Expanded(
-            child: Consumer<TransactionsProvider>(
-              builder: (context, TransactionsProvider provider, _) {
-                if(provider.ordersList != null && provider.ordersList.data.length == 0) {
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            FutureBuilder<TransactionListResponse>(
+              future: provider.loadOrdersList(context),
+              builder: (context, AsyncSnapshot<TransactionListResponse> snapshot) {
+                if(snapshot.connectionState == ConnectionState.waiting){
+                  return Center(child: CircularProgressIndicator());
+                }
+
+                if(snapshot.hasError){
+                  return Center(child: Text(snapshot.error.toString()));
+                }
+
+                TransactionListResponse response = snapshot.data;
+
+                if(response.data != null && response.data.length == 0) {
                   return Center(child: Text("Não há pedidos ainda :)"));
                 }
 
-                return ListView.builder(
-                  padding: const EdgeInsets.all(8),
-                  itemCount: provider.ordersList != null ? provider.ordersList.data.length : 0,
-                  itemBuilder: (context, i) => TransactionListTile(provider.ordersList.data[i], false),
+                return Expanded(
+                  child: ListView.builder(
+                    itemCount: provider.ordersList != null ? provider.ordersList.data.length : 0,
+                    itemBuilder: (context, i) => TransactionListTile(provider.ordersList.data[i], false),
+                  ),
                 );
               },
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
