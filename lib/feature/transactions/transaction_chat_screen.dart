6fcd0fe474c0a346f60008c2f1d8b0b27ad8d6b4
George Rappel <george.concei@hotmail.com>
2020-12-02 18:57:11 -0300

    + Transaction Chat Screen placeholder
    Took 14 minutes

---
+++ lib/feature/transactions/transaction_chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:trocado_flutter/widget/trocado_app_bar.dart';
+
+class TransactionChatScreen extends StatefulWidget {
+  @override
+  _TransactionChatScreenState createState() => _TransactionChatScreenState();
+}
+
+class _TransactionChatScreenState extends State<TransactionChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: trocadoAppBar("Chat"),
+      body: Column(
+        children: [
+          Text("oiv"),
+        ],
+      ),
+    );
+  }
+}
