6cff9a9a8d7fe4043bdaa3a6012baf29e6f605c0
George Rappel <george.concei@hotmail.com>
2020-12-23 20:22:04 -0300

    * Blog - fix formatting
    Took 6 minutes

--- lib/feature/blog/blog_post_screen.dart
+++ lib/feature/blog/blog_post_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
+import 'package:trocado_flutter/config/style.dart';
 import 'package:trocado_flutter/widget/trocado_app_bar.dart';
 
 import 'blog_post.dart';
@@ -15,19 +16,26 @@ class BlogPostScreen extends StatelessWidget {
       body: Column(
         mainAxisSize: MainAxisSize.max,
         children: [
-          Hero(tag: post.title, child: Image.network(post.imageUrl)),
-          ListTile(
-            title: Text(post.title),
-            subtitle: Text(post.creationDate),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Html(data: post.content),
-          ),
-          ListTile(
-            leading: CircleAvatar(backgroundImage: NetworkImage(post.authorImageUrl)),
-            title: Text(post.authorName),
-            subtitle: const Text("Autor"),
+          Expanded(
+            child: ListView(
+              children: [
+                Hero(tag: post.title, child: Image.network(post.imageUrl)),
+                ListTile(
+                  title: Text(post.title, style: const TextStyle(fontSize: 20),),
+                  subtitle: Text(post.creationDate),
+                ),
+                const Divider(color: Style.accentColor, height: 4, indent: 12, endIndent: 12),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: Html(data: post.content),
+                ),
+                ListTile(
+                  leading: post.authorImageUrl == null ? null : CircleAvatar(backgroundImage: NetworkImage(post.authorImageUrl)),
+                  title: Text(post.authorName),
+                  subtitle: const Text("Autor"),
+                ),
+              ],
+            ),
           ),
         ],
       ),
