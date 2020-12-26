9806d46804ff3a2ec4f97e00f127726003c77c04
George Rappel <george.concei@hotmail.com>
2020-12-26 18:22:03 -0300

    * Solve unused warnings
    Took 4 minutes

--- lib/feature/blog/blog_list_screen.dart
+++ lib/feature/blog/blog_list_screen.dart
@@ -1,13 +1,11 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
-import 'package:trocado_flutter/api/api_helper.dart';
-import 'package:trocado_flutter/feature/blog/blog_post.dart';
-import 'package:trocado_flutter/widget/trocado_app_bar.dart';
 import 'package:http/http.dart' as http;
+import 'package:trocado_flutter/feature/blog/blog_post.dart';
+import 'package:trocado_flutter/feature/blog/blog_post_screen.dart';
+import 'package:trocado_flutter/widget/trocado_app_bar.dart';
 
-import 'blog_post_screen.dart';
 
 class BlogListScreen extends StatefulWidget {
   @override
@@ -16,11 +14,9 @@ class BlogListScreen extends StatefulWidget {
 
 class _BlogListScreenState extends State<BlogListScreen> {
   Future<List<BlogPost>> loadPosts() async {
-    ApiHelper apiHelper = ApiHelper();
     String url =
         "https://api.cosmicjs.com/v1/dc3f5e80-432a-11eb-8b3b-c59fea007426/objects?hide_metafields=true&type=posts&read_key=hSFUdhJeuLlkBtSxdV957Jn5hFvnPGYDgLtgHV9Y9zeGKBqy9S&limit=30&props=slug,title,content,metadata,created_at";
     http.Response response = await http.get(url, headers: {"charset": "utf-8", "Accept-Charset": "utf-8"});
-    // print('Body: ' + utf8.decode(response.bodyBytes));
     dynamic responseJson = json.decode(utf8.decode(response.bodyBytes));
     List<BlogPost> posts = [];
     for (dynamic post in responseJson['objects']) {
